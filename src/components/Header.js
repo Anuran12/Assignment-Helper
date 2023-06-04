f1406e7c5f72e4bfca1f701f5b6d8acc9fcdb081
Anuran12 <anuranarts@gmail.com>
2023-06-04 11:41:21 +0530

    ask question and answer modal set

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../logo.gif';
 import HomeIcon from '@material-ui/icons/Home';
 import FeaturedPlayListOutlinedIcon from '@material-ui/icons/FeaturedPlayList';
@@ -10,12 +10,19 @@ import LanguageIcon from "@material-ui/icons/Language";
 
 // import Modal from "react-modal";
 
-
+import 'react-responsive-modal/styles.css'
 import "./css/Header.css";
 import { Avatar, Button, Input } from "@material-ui/core";
+import Modal from 'react-responsive-modal';
+import CloseIcon from "@material-ui/icons/Close";
+import { ExpandMore, PeopleAltOutlined } from '@material-ui/icons';
+
 
 
 function Header() {
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const [inputUrl, setinputUrl] = useState("")
+    const Close = (<CloseIcon />)
   return (
     <div className='qHeader'>
         <div className='qHeader-content'>
@@ -46,7 +53,66 @@ function Header() {
                 <div className='qHeader__Rem'>
                     <Avatar />
                 </div>
-                <Button>Add Question</Button>
+                <Button onClick={() => setIsModalOpen(true)}>Add Question</Button>
+                <Modal
+                open = {isModalOpen}
+                closeIcon={Close}
+                onClose={() => setIsModalOpen(false)}
+                closeOnEsc
+                center
+                closeOnOverlayClick={false}
+                styles={{
+                    overlay: {
+                        height: "auto",
+                    },
+                }}
+                >
+                    <div className='modal__title'>
+                        <h5>Add Question</h5>
+                        <h5>Share Link</h5>
+                    </div>
+                    <div className='modal__info'>
+                        <Avatar src='' className='avatar'/>
+                        <div className='modal__scope'>
+                            <PeopleAltOutlined />
+                            <p>Public</p>
+                            <ExpandMore />
+                        </div>
+                    </div>
+                    <div className='modal__Field'>
+                        <Input type="text" placeholder= "Start your question with 'What', 'How', 'Why', etc." />
+                        <div style={{
+                            display: "flex",
+                            flexDirection: "column"
+                        }}>
+                            <input type='text'
+                            value={inputUrl}
+                            onChange={(e) => setinputUrl(e.target.value)}
+                            style={{
+                            margin:"5px 0",
+                            border: "1px solid lightgray",
+                            padding: "10px",
+                            outline: "2px solid #000"
+                            
+                        }} placeholder='Optional: include a link that gives context' />
+                        {
+                            inputUrl !== "" && <img style={{
+                                height: "40vh",
+                                objectFit: "contain",
+                            }} src={inputUrl} alt='display image' />
+                        }
+                        
+                        </div>
+                    </div>
+                    <div className='modal__buttons'>
+                        <button className='cancle' onClick={() => setIsModalOpen(false)}>
+                            Cancel
+                        </button>
+                        <button type='submit' className='add'>
+                            Add Question
+                        </button>
+                    </div>
+                </Modal>
         </div>
     </div>
   )
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,79 +1,115 @@
 import { Avatar } from "@material-ui/core";
-import React from 'react';
+import React, { useState } from 'react';
 import './css/Post.css';
 import { ArrowDownwardOutlined, ArrowUpwardOutlined, ChatBubbleOutlined, MoreHorizOutlined, RepeatOutlined, ShareOutlined } from "@material-ui/icons";
+import Modal from 'react-responsive-modal';
+import 'react-responsive-modal/styles.css';
+import 'react-quill/dist/quill.snow.css';
+import CloseIcon from "@material-ui/icons/Close";
+import ReactQuill from "react-quill";
 
 function Post() {
-  return (
-    <div className='post'>
-        <div className='post__info'>
-            <Avatar src="" />
-            <h4>User Name</h4>
-            <small>Timestamp</small>
-        </div>
-        <div className="post__body">
-            <div className="post__question">
-            <p>
-                This is test Question
-            </p>
-            <button className="post__btnAnswer">Answer</button>
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const Close = (<CloseIcon />)
+    return (
+        <div className='post'>
+            <div className='post__info'>
+                <Avatar src="" />
+                <h4>User Name</h4>
+                <small>Timestamp</small>
             </div>
-        </div>
-        <div className="post__footer">
-            <div className="post__footerAction">
-                <ArrowUpwardOutlined />
-                <ArrowDownwardOutlined />
+            <div className="post__body">
+                <div className="post__question">
+                <p>
+                    This is test Question
+                </p>
+                <button onClick={() => setIsModalOpen(true)} className="post__btnAnswer">Answer</button>
+                <Modal
+                open = {isModalOpen}
+                closeIcon={Close}
+                onClose={() => setIsModalOpen(false)}
+                closeOnEsc
+                center
+                closeOnOverlayClick={false}
+                styles={{
+                    overlay: {
+                        height: "auto",
+                    },
+                }}
+                >
+                    <div className="modal__question">
+                        <h1>This is test question</h1>
+                        <p>asked by {" "} <span className="name">Username</span> on <span className="name">Timestamp</span></p>
+                    </div>
+                    <div className="modal__answer">
+                        <ReactQuill placeholder="Enter your answer" />
+                    </div>
+                    <div className="modal__button">
+                    <button className='cancle' onClick={() => setIsModalOpen(false)}>
+                            Cancel
+                        </button>
+                        <button type='submit' className='add'>
+                            Add Question
+                        </button>
+                    </div>
+                </Modal>
+                </div>
             </div>
-            <RepeatOutlined />
-            <ChatBubbleOutlined />
-            <div className="post__footerLeft">
-                <ShareOutlined />
-                <MoreHorizOutlined />
+            <div className="post__footer">
+                <div className="post__footerAction">
+                    <ArrowUpwardOutlined />
+                    <ArrowDownwardOutlined />
+                </div>
+                <RepeatOutlined />
+                <ChatBubbleOutlined />
+                <div className="post__footerLeft">
+                    <ShareOutlined />
+                    <MoreHorizOutlined />
+                </div>
             </div>
-        </div>
-        <p style={{
-            color: "rgba(0,0,0,0.5)",
-            fontSize: "12px",
-            fontWeight: "bold",
-            margin: "10px 0",
-        }}>1 Answer</p>
-        <div style={{
-            margin: "5px 0px 0px 0px",
-            padding: "5px 0px 0px 20px",
-            borderTop: "1px solid lightgray", 
-        }} className="post__answer">
+            <p style={{
+                color: "rgba(0,0,0,0.5)",
+                fontSize: "12px",
+                fontWeight: "bold",
+                margin: "10px 0",
+            }}>1 Answer</p>
             <div style={{
-                display: "flex",
-                flexDirection: "column",
-                width: "100%",
-                padding: "10px 5px",
-                borderTop: "1px solid lightgray",
-            }} className="post-answer-container">
+                margin: "5px 0px 0px 0px",
+                padding: "5px 0px 0px 20px",
+                borderTop: "1px solid lightgray", 
+            }} className="post__answer">
                 <div style={{
                     display: "flex",
-                    alignItems: "center",
-                    marginBottom: "10px",
-                    fontSize: "12px",
-                    fontWeight: 600,
-                    color: "#888",
-                }} className="post-answered">
-                    <Avatar />
+                    flexDirection: "column",
+                    width: "100%",
+                    padding: "10px 5px",
+                    borderTop: "1px solid lightgray",
+                }} className="post-answer-container">
                     <div style={{
-                        margin: "0px 10px",
-                    }} className="post-info">
-                        <p>
-                            Username
-                        </p>
-                        <span>Timestamp</span>
+                        display: "flex",
+                        alignItems: "center",
+                        marginBottom: "10px",
+                        fontSize: "12px",
+                        fontWeight: 600,
+                        color: "#888",
+                    }} className="post-answered">
+                        <Avatar />
+                        <div style={{
+                            margin: "0px 10px",
+                        }} className="post-info">
+                            <p>
+                                Username
+                            </p>
+                            <span>Timestamp</span>
+                        </div>
                     </div>
-                </div>
-                <div className="post-answer">
-                    This is test answer
+                    <div className="post-answer">
+                        This is test answer
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Post
